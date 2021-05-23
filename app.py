0a2f83610f7ffd83c780e7024ad751ba96e442ad
Seungho Kang <will.seungho@gmail.com>
2021-05-23 12:56:20 +0900

    주식 현재가 조회시, 한 주식이 조회가 안되면 에러가 아닌 빈 정보로 반환.

--- app.py
+++ app.py
@@ -7,22 +7,24 @@ app = Flask(__name__)
 
 
 @app.route('/ping')
-def hello_world():
-    return 'OK'
+def ping():
+    return 'pong'
 
 
 @app.route('/api/v1/stock/code')
-def getDomesticCode():
+def getListedStockCodes():
     type = request.args.get('type')
     if type not in ['KRX', 'NASDAQ']:
         abort(400, 'Bad Type')
+
     if type == 'KRX':
-        return jsonify(getKRXCodes())
+        return jsonify(getKrxCodes())
+
     df = fdr.StockListing('NASDAQ')[['Symbol', 'Name']]
     return Response(df.to_json(orient='records'), mimetype='application/json')
 
 
-def getKRXCodes():
+def getKrxCodes():
     result = []
     for ticker in stock.get_market_ticker_list(market='ALL'):
         name = stock.get_market_ticker_name(ticker)
@@ -34,21 +36,30 @@ def getKRXCodes():
 
 
 @app.route('/api/v1/stock/price')
-def getPrice():
-    codes = request.args.get('codes').split(",")
+def getCurrentStockPrice():
+    codes = request.args.get('codes')
+    codes = distinctAndSplit(codes)
     result = []
-
+    day = date.today() - timedelta(days=14)
     for code in codes:
-        today = date.today()
-        day = today - timedelta(days=14)
-        if fdr.DataReader(code, day).empty:
-            abort(404, 'Not Found Stock')
-        df = fdr.DataReader(code, day)[['Close']]
-        result.append({
-            "code": code,
-            "price": str(df.iloc[-1][0])
-        })
+        current_price = fetchCurrentStockPrice(code, day)
+        if current_price:
+            result.append(fetchCurrentStockPrice(code, day))
     return jsonify(result)
+
+
+def distinctAndSplit(codes):
+    return list(set(codes.split(",")))
+
+
+def fetchCurrentStockPrice(code, day):
+    if fdr.DataReader(code, day).empty:
+        return False
+    df = fdr.DataReader(code, day)[['Close']]
+    return {
+        "code": code,
+        "price": str(df.iloc[-1][0])
+    }
 
 
 if __name__ == '__main__':
