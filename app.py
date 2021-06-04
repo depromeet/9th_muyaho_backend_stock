2815d31a7c64edff2ff791359bdae63fa710451c
Seungho Kang <will.seungho@gmail.com>
2021-06-04 23:11:01 +0900

    [MH-2] 코드 정리

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ from datetime import date, timedelta
 app = Flask(__name__)
 
 
+# Health Check
 @app.route('/ping')
 def ping():
     return 'pong'
 
 
+allowed_stock_type = ['KRX', 'NASDAQ']
+
+
+# 상장중인 종목 정보를 불러오는 API (국내 주식, 해외 주식)
 @app.route('/api/v1/stock/code')
 def getListedStockCodes():
     type = request.args.get('type')
-    if type not in ['KRX', 'NASDAQ']:
+    if type not in allowed_stock_type:
         abort(400, 'Bad Type')
 
     if type == 'KRX':
@@ -25,24 +30,24 @@ def getListedStockCodes():
 
 
 def getKrxCodes():
-    result = []
+    krx_stock_codes = []
     for ticker in stock.get_market_ticker_list(market='ALL'):
         name = stock.get_market_ticker_name(ticker)
-        result.append({
+        krx_stock_codes.append({
             "Symbol": ticker,
             "Name": name
         })
-    return result
+    return krx_stock_codes
 
 
+# 해당하는 주식들의 현재가를 조회하는 API
 @app.route('/api/v1/stock/price')
 def getCurrentStockPrice():
     codes = request.args.get('codes')
-    codes = distinctAndSplit(codes)
     result = []
-    day = date.today() - timedelta(days=14)
-    for code in codes:
-        current_price = fetchCurrentStockPrice(code, day)
+    last_day = date.today() - timedelta(days=14)
+    for code in distinctAndSplit(codes):
+        current_price = fetchCurrentStockPrice(code, last_day)
         if current_price:
             result.append(current_price)
     return jsonify(result)
@@ -53,13 +58,12 @@ def distinctAndSplit(codes):
 
 
 def fetchCurrentStockPrice(code, day):
-    stock = fdr.DataReader(code, day)
-    if stock.empty:
+    current_stock_price = fdr.DataReader(code, day)
+    if current_stock_price.empty:
         return False
-    df = stock[['Close']]
     return {
         "code": code,
-        "price": str(df.iloc[-1][0])
+        "price": str(current_stock_price[['Close']].iloc[-1][0])
     }
 
 
