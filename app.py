843a8da29bc94e66f26f0cf8a7a38d2af20b6d3b
Seungho Kang <will.seungho@gmail.com>
2021-06-07 01:32:31 +0900

    [MH-2] 주식 정보를 실시간으로 불러오지 못하는 경우 읻단 -1을 반환하도록.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, abort, jsonify, Response
 import FinanceDataReader as fdr
 from pykrx import stock
 from datetime import date, timedelta
+import logging
 
 app = Flask(__name__)
 
@@ -58,13 +59,20 @@ def distinctAndSplit(codes):
 
 
 def fetchCurrentStockPrice(code, day):
-    current_stock_price = fdr.DataReader(code, day)
-    if current_stock_price.empty:
-        return False
-    return {
-        "code": code,
-        "price": str(current_stock_price[['Close']].iloc[-1][0])
-    }
+    try:
+        current_stock_price = fdr.DataReader(code, day)
+        if current_stock_price.empty:
+            return False
+        return {
+            "code": code,
+            "price": str(current_stock_price[['Close']].iloc[-1][0])
+        }
+    except Exception as e:
+        logging.error("주식 현재가를 불러오는 중 에러가 발생하였습니다 code: {0} error: {1}".format(code, e))
+        return {
+            "code": code,
+            "price": -1
+        }
 
 
 if __name__ == '__main__':
